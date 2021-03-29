abd7da87647dc172ff936f21b1a872761427a8c1
Amanda Martins <80703615+amandamartinsd@users.noreply.github.com>
2021-03-29 19:54:03 -0300

    Add files via upload

---
+++ meu-primeiro-conversoramd/README.markdown
@@ -0,0 +1,5 @@
+# Meu primeiro Conversor - AMD
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/amandamartinsd/pen/oNBNzmg](https://codepen.io/amandamartinsd/pen/oNBNzmg).
+
+Primeiro código com conversor de idade de cachorros hehe
---
+++ meu-primeiro-conversoramd/dist/script.js
@@ -0,0 +1,14 @@
+var valorIdadeTexto = prompt("Qual a idade (humana) do seu cachorro?")
+
+var valorIdadeNumero = parseFloat(valorIdadeTexto)
+
+var valorIdade = valorIdadeNumero * 7
+var valorIdadeFixado = valorIdade
+
+alert(valorIdadeFixado)
+
+//Revisão
+//var - variáveis, podemos guardar números inteiros, casas decimais, strings entre as aspas que digitamos qualquer texto para mostrar na tela (int - float - string)
+//alert - parseInt(inteiros) - parseFloat (decimais) - prompt (ele manda um alerta, mas recebe informação também)
+//operações + somar * multiplicar
+//comentário final
